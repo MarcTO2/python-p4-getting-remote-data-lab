fb16f37af67ae490cbff08a1450846f0bd0978d4
Absalom <mrclabsalom@gmail.com>
2023-10-02 06:34:36 +0300

    Completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+        response = requests.get(URL)
+        return response.content
         pass
 
     def load_json(self):
+        response_content = self.get_programs()
+        try:
+            # Attempt to decode the response content as JSON
+            json_data = json.loads(response_content.decode('utf-8'))
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {str(e)}")
+            return None
         pass
